d59754ca92cad9a1557f47a1923a7d77667e9c6c
vimal prakash <vimalp015@gmail.com>
2016-09-02 02:51:06 +1000

    First commit

---
+++ WeatherReport.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WeatherReport.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
